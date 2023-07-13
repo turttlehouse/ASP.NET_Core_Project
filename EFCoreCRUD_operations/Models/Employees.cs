6452033bb28ee111dddb4306752c8726306406dd
turttlehouse <buildandrun464@gmail.com>
2023-07-13 20:15:39 +0545

    employee.cs file updated

--- EFCoreCRUD_operations/Models/Employees.cs
+++ EFCoreCRUD_operations/Models/Employees.cs
@@ -1,6 +1,14 @@
-﻿namespace EFCoreCRUD_operations.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFCoreCRUD_operations.Models
 {
     public class Employees
     {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int salary { get; set; }
     }
 }
