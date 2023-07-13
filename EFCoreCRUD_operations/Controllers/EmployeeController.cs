dc82b3972fc9b11cdf2f33905e1c15c5dc6fe5ea
turttlehouse <buildandrun464@gmail.com>
2023-07-13 21:24:00 +0545

    The registered dependency injection is injected inside the newly created controller i.e Employee controller

---
+++ EFCoreCRUD_operations/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using EFCoreCRUD_operations.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFCoreCRUD_operations.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ApplicationContext context;
+
+        public EmployeeController(ApplicationContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
