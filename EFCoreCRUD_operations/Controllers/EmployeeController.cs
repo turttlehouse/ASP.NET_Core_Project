506578abb58002be1dcc44e4a6ae25604826b1f3
turttlehouse <buildandrun464@gmail.com>
2023-08-04 08:10:40 +0545

    null check added for employee before deleting and updating value of employees.

--- EFCoreCRUD_operations/Controllers/EmployeeController.cs
+++ EFCoreCRUD_operations/Controllers/EmployeeController.cs
@@ -1,112 +1,106 @@
 ﻿using EFCoreCRUD_operations.Data;
 using EFCoreCRUD_operations.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Identity.Client;
+using System.IO;
 
-namespace EFCoreCRUD_operations.Controllers
+public class EmployeeController : Controller
 {
-    public class EmployeeController : Controller
+    private readonly ApplicationContext context;
+
+    public EmployeeController(ApplicationContext context)
     {
-        private readonly ApplicationContext context;
+        this.context = context;
+    }
 
-        public EmployeeController(ApplicationContext context)
-        {
-            this.context = context;
-        }
-       
-        public IActionResult Index()
-        {
-            var tabledata = context.Employees.ToList();
+    public IActionResult Index()
+    {
+        var tabledata = context.Employees.ToList();
+        return View(tabledata);
+    }
 
-            return View(tabledata);
-        }
-        [HttpGet]
-        public IActionResult Create()
-        {
-            return View();  
-        }
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
 
-        [HttpPost]
-
-        public IActionResult Create(Employees model)
+    [HttpPost]
+    public IActionResult Create(Employees model)
+    {
+        if (ModelState.IsValid)
         {
-            if (ModelState.IsValid)
+            var data = new Employees
             {
-                var data = new Employees
-                {
-                    Name = model.Name,
-                    salary = model.salary,
-
-                };
-                context.Employees.Add(data);
-                context.SaveChanges();
-
-                return RedirectToAction("Index");
-
-            }
-            else
-            {
-                return View(model);
-            }
-
-
-            //var data = new Employees                    // for inserting our predefined data directly to create the data
-            //{
-            //    Name = "John",
-            //    salary = 5000,
-            //};
-            //context.Employees.Add(data);
-            //context.SaveChanges();
-
-            //return RedirectToAction("Index");
-
+                Name = model.Name,
+                salary = model.salary,
+            };
+            context.Employees.Add(data);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        else
+        {
+            return View(model);
         }
 
-        public IActionResult Delete(int id)
+        // Check if the predefined data already exists in the database
+        // if (!context.Employees.Any(e => e.Name == "John"))  /*Any(e => e.Name == "John"): This is a LINQ query using the Any method. It checks if there is any record in the EmployeeTable DbSet where the Name property is equal to "John".*/
+        //{
+        //    var data = new Employees // for inserting our predefined data directly to create the data
+        //    {
+        //        Name = "John",
+        //        salary = 5000,
+        //    };
+        //    context.Employees.Add(data);
+        //    context.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+        //else
+        //{
+        //    return RedirectToAction("Index");
+        //}
+
+    }
+
+    public IActionResult Delete(int id)
+    {
+        var emp = context.Employees.SingleOrDefault(x => x.Id == id);
+        if (emp != null)
         {
-            var emp = context.Employees.SingleOrDefault(X=> X.Id == id);
             context.Employees.Remove(emp);
             context.SaveChanges();
-            return RedirectToAction("Index");   
         }
+        return RedirectToAction("Index");
+    }
 
-        public IActionResult Update(int id)
+    public IActionResult Update(int id)
+    {
+        var emp = context.Employees.SingleOrDefault(x => x.Id == id);
+        var result = new Employees()
         {
-            var emp = context.Employees.SingleOrDefault(x=> x.Id == id);
-            var result = new Employees()
-            {
-                Name = emp.Name,
-                salary = emp.salary,
-            };
-           
+            Name = emp.Name,
+            salary = emp.salary,
+        };
+        return View(result);
+    }
 
-           
-
-            return View(result);  
-        }
-
-        [HttpPost]
-
-        public IActionResult Update(Employees model)
+    [HttpPost]
+    public IActionResult Update(Employees model)
+    {
+        if (ModelState.IsValid)
         {
-            if (ModelState.IsValid)
+            var emp = context.Employees.SingleOrDefault(x => x.Id == model.Id);
+            if (emp != null)
             {
-                var data = new Employees
-                {
-                    Name = model.Name,
-                    salary = model.salary,
-
-                };
-                context.Employees.Add(data);
+                emp.Name = model.Name;
+                emp.salary = model.salary;
                 context.SaveChanges();
-
-                return RedirectToAction("Index");
-
             }
-            else
-            {
-                return View(model);
-            }
+            return RedirectToAction("Index");
+        }
+        else
+        {
+            return View(model);
         }
     }
 }
